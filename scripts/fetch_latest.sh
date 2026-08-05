de00ec7039dee7188a65cc9809eb0476f75a6519
Diogo Azevedo <hi@dio-az.dev>
2026-08-05 19:57:10 +0200

    ci: ensure fetch_latest returns the correct version

--- scripts/fetch_latest.sh
+++ scripts/fetch_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-filename=$(wget -qO- "https://$1" | grep -oE "$2(\.[0-9]+)?\.tar\.gz" | sort -V | tail -n1)
-destination="${filename%%-*}"
+package="${2%%-*}"
+version=$(wget -qO- "https://$1" | grep -oE "$2(\.[0-9]+)*\.tar\.gz" | sed -E "s/^$package-(.*)\.tar\.gz$/\1/" | sort -V | tail -n1)
 
-mkdir -p "$destination"
-wget -qO- "https://$1/$filename" | tar -xz --strip-components 1 -C "$destination"
+mkdir -p "$package"
+wget -qO- "https://$1/$package-$version.tar.gz" | tar -xz --strip-components 1 -C "$package"
